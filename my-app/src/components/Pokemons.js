290ae4e2b17a4f5cba1418602e2639bf96b2592f
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-09-09 14:28:19 +0200

    add range filter

--- my-app/src/components/Pokemons.js
+++ my-app/src/components/Pokemons.js
@@ -6,6 +6,7 @@ const Pokemons = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
   const [selectedRadio, setSelectedRadio] = useState("");
+  const [rangeValue, setRangeValue] = useState(20);
   const [searchTerm, setSearchTerm] = useState("");
   const filter = [];
 
@@ -45,6 +46,16 @@ const Pokemons = () => {
             setSearchTerm(e.target.value);
           }}
         />
+        <input
+          type="range"
+          name="range"
+          min={0}
+          max={30}
+          value={rangeValue}
+          onChange={(e) => {
+            setRangeValue(e.target.value);
+          }}
+        />
         <ul className="radio-container">
           {data.map((pokemons) => {
             filter.push(...pokemons.types);
@@ -69,20 +80,22 @@ const Pokemons = () => {
               </li>
             ))}
         </ul>
+        {(selectedRadio || searchTerm) && (
+          <button onClick={() => setSelectedRadio("")}>
+            Annuler la recherche
+          </button>
+        )}
       </div>
-      {(selectedRadio || searchTerm) && (
-        <button onClick={() => setSelectedRadio("")}>
-          Annuler la recherche
-        </button>
-      )}
       <div className="pokemons-container">
         {selectedRadio
           ? data
               .filter((pokemons) => pokemons.name.includes(searchTerm))
               .filter((pokemons) => pokemons.types.includes(selectedRadio))
+              .slice(0, rangeValue)
               .map((pokemons) => <Card key={pokemons.id} pokemons={pokemons} />)
           : data
               .filter((pokemons) => pokemons.name.includes(searchTerm))
+              .slice(0, rangeValue)
               .map((pokemons) => (
                 <Card key={pokemons.id} pokemons={pokemons} />
               ))}
