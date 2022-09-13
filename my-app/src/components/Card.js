d8ec9a95fc21ac13d394c0db42685db7b88d937e
Thibaut Dusautoir <tdusautoir@myges.fr>
2022-09-13 23:57:17 +0200

    add input for hp and cp

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-const Card = ({ pokemons }) => {
+const Card = ({ pokemons }, types) => {
   const [disabled, setDisabled] = useState(true);
   const [editable, setEditable] = useState(false);
   const [name, setName] = useState(pokemons.name);
+  const [hp, setHp] = useState(pokemons.hp);
+  const [cp, setCp] = useState(pokemons.cp);
 
   const SendChange = () => {
     setEditable(false);
     setDisabled(true);
     console.log("modif : " + name);
+    console.log("hp : " + hp);
+    //need to check error
+    if (cp > 30) {
+      console.error("cp must be under 30.");
+    } else {
+      console.log("cp : " + cp);
+    }
   };
 
   return (
@@ -39,18 +48,35 @@ const Card = ({ pokemons }) => {
         }}
         type="text"
         className="name"
+        maxLength={20}
         value={name}
         disabled={disabled}
       />
       <div className="spec">
         <div className="hp">
-          <p>pv</p>
-          {pokemons.hp}
+          <p>hp</p>
+          <input
+            onChange={(e) => {
+              setHp(e.target.value);
+            }}
+            max={30}
+            type="number"
+            value={hp}
+            disabled={disabled}
+          />
         </div>
         <div className="separator"></div>
         <div className="cp">
           <p>cp</p>
-          {pokemons.cp}
+          <input
+            onChange={(e) => {
+              setCp(e.target.value);
+            }}
+            type="number"
+            max={30}
+            value={cp}
+            disabled={disabled}
+          />
         </div>
       </div>
       <div className="type">
